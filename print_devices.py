0ec5882b693ad00c53d9eb99cbc6f04c8d8d4b8e
Forrest Smith <fasmith0229@gmail.com>
2018-04-22 20:02:25 -0500

    print_devices.py and print_hosts.py have been updated to utilize module functions in apic_em.py.  Since this is now the case, the user is prompted for a password any time a new connection is established.  You can always call get_session to keep a session open until it is no longer needed.

--- print_devices.py
+++ print_devices.py
@@ -1,58 +1,23 @@
 import requests
-import json
+from apic_em import get_devices
 from tabulate import tabulate
 
 # Disable SSL warnings
-requests.packages.urllib3.disable_warnings()  
-
-
-def get_ticket():
-    '''
-    get_ticket: retrieves and returns a service ticket (token)
-                from the APIC-EM Sandbox
-    '''
-    main_api = "https://sandboxapicem.cisco.com/api/v1/ticket"
-    headers = {"content-type": "application/json"}
-    body_json = {
-        "username": "devnetuser",
-        "password": "Cisco123!",
-    }
-    resp = requests.post(main_api,
-                         json.dumps(body_json),
-                         headers=headers,
-                         verify=False)
-    print("Ticket request status: ", resp.status_code)
-    response_json = resp.json()
-    serviceTicket = response_json["response"]["serviceTicket"]
-    return serviceTicket
+requests.packages.urllib3.disable_warnings()
 
 
 def print_devices():
-    api_url = "https://sandboxapicem.cisco.com/api/v1/network-device"
-    ticket = get_ticket()
-    headers = {
-        "content-type": "application/json",
-        "X-Auth-Token": ticket
-    }
+    """Prints network devices and IP addresses. Note, uses get_devices
+    """
+    device_table = []
+    for n, device in enumerate(get_devices()):
+        device_table.append(
+            [n,
+             device.type,
+             device.ip
+             ])
+    table_header = ["Number", "Type", "Management IP"]
+    print(tabulate(device_table, table_header))
 
-    resp = requests.get(api_url, headers=headers, verify=False)
-    print("Status of /network-device request: ", resp.status_code)
-    if resp.status_code != 200:
-        raise Exception(
-            "Status code does not equal 200. Response text: " + resp.text)
-    response_json = resp.json()
-
-    device_list = []
-    i = 0
-    for item in response_json["response"]:
-        i += 1
-        device = [
-            i,
-            item["type"],
-            item["managementIpAddress"]
-        ]
-        device_list.append(device)
-        table_header = ["Number", "Type", "Management IP"]
-    print(tabulate(device_list, table_header))
 
 print_devices()
